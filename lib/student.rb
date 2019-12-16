4653cb64932a642a814a56966d754d0679509da1
allison_johnson <allison.cuttler@gmail.com>
2019-12-16 12:39:20 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end #init
+
+  def learn(str)
+    @knowledge << str 
+  end #learn
+
+  def knowledge
+    @knowledge 
+  end #knowlege
 
 end
