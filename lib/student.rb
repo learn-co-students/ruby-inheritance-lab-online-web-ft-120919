3e39f14f0159546e30fe70a449bec1c7df2fcd50
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-09 01:39:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+attr_accessor :first_name, :last_name
+
+def initialize
+  @knowledge = []
+end 
+
+def learn(string_knowledge)
+  @knowledge << string_knowledge
+end 
+
+def knowledge
+  @knowledge
+end 
 end
